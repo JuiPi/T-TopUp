0cb585233e2c1c6bf7d32f4d3ed70ddb2dc5f50a
VN140 <vichayuth.nga@student.mahidol.ac.th>
2024-03-21 02:17:37 +0700

    Add Homepage file tsx

---
+++ T-TopUp/src/Homepage.tsx
@@ -0,0 +1,22 @@
+function Homepage() {
+    return (
+        <>
+            <body>
+                <header>
+                    <nav className="nav1">
+                        <a href="#">Log in</a>
+                    </nav>
+                    <nav className="nav2">
+                        <a href="Homepage.tsx">
+                            <img src="image.png" alt="Logo" className="logosize"></img>
+                        </a>
+                        <a href="Homepage.tsx">Home</a>
+                        <a href="#">Games</a>
+                        <a href="#">User Management</a>
+                        <a href="#">Product Management</a>
+                    </nav>
+                </header>
+            </body>
+        </>
+    )
+}
