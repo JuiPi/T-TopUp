d70013ad8ade4d8c832f05ef9d72be264f7e92e8
VN140 <vichayuth.nga@student.mahidol.ac.th>
2024-03-22 16:12:37 +0700

    Edit Homepage

--- T-TopUp/src/Homepage.tsx
+++ T-TopUp/src/Homepage.tsx
@@ -1,23 +1,39 @@
+import picture from "./logo.png";
+import ad1 from "./Game icon/Valorant/Val_banner.png"
+import ad2 from "./Game icon/TF2/tf2_banner.png"
+import ad3 from "./Game icon/OW/OW_banner.png"
+
 function Homepage() {
-    return (
-        <>
-            <body>
-                <header>
-                    <nav className="nav1">
-                        <a href="#">Log in</a>
-                    </nav>
-                    <nav className="nav2">
-                        <a href="Homepage.tsx">
-                            <img src="image.png" alt="Logo" className="logosize"></img>
-                        </a>
-                        <a href="Homepage.tsx">Home</a>
-                        <a href="#">Games</a>
-                        <a href="#">User Management</a>
-                        <a href="#">Product Management</a>
-                    </nav>
-                </header>
-            </body>
-        </>
-    )
+  return (
+    <>
+      <body>
+        <header>
+          <nav className="nav1">
+            <a href="#">Log in</a>
+          </nav>
+          <nav className="nav2">
+            <a href="Homepage.tsx">
+              <img src={picture} alt="Logo" className="logosize"></img>
+            </a>
+            <a href="Homepage.tsx">Home</a>
+            <a href="#">Games</a>
+            <a href="#">User Management</a>
+            <a href="#">Product Management</a>
+          </nav>
+          <section>
+            <div className="slider">
+                <div className="slides">
+                    <img id="slide-1" src={ad1} alt="ad1"></img>
+                    <img id="slide-2" src={ad2} alt="ad2"></img>
+                    <img id="slide-3" src={ad3} alt="ad3"></img>
+                    <a className="prev">&#10094</a>
+                    <a className="next">&#10095</a>
+                </div>
+            </div>
+          </section>
+        </header>
+      </body>
+    </>
+  );
 }
 export default Homepage;
--- T-TopUp/src/main.tsx
+++ T-TopUp/src/main.tsx
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import Homepage from './Homepage.tsx'
 import Aboutus from './Aboutus.tsx'
-import './index.css'
-import './App.css'
 import './Homepage.css'
 import './Aboutus.css'
 
