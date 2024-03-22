851882f536e4d4b498df63595f2315cdb5bd5898
PUNNAPRAN <109496465+PUNNAPRAN@users.noreply.github.com>
2024-03-23 02:05:49 +0700

    UPDATE ABOUT US

--- T-TopUp/src/Aboutus.tsx
+++ T-TopUp/src/Aboutus.tsx
@@ -17,8 +17,8 @@ function Aboutus(){
             <a href="#">Product Management</a>
         </nav>
     </header>
-    
-    <body>
+{/*  */}
+  <body>
       <div>
           <p className="TeamMem">
             Our Team Members
@@ -27,29 +27,112 @@ function Aboutus(){
 
       <div className="container">
             <div className="grid-container">
+{/*  */}
+        <div className="column">
+          <div className="Picadjust">
+            <div className="PicFrame">
+              {/* img Term */}
+            </div>
+          </div>
+            <p className="NameTeamM">Term</p>
+              <div className="PersonName">
+                <p className="GapName">Phusit</p>
+                  <p className="GapName">Mongkhonwatcharaphun</p>
+              </div>
 
-                <div className="column">
-                    <p className="NameTeamM">Column 1</p>
-                  </div>
-                <div className="column">
-                    <p className="NameTeamM">Column 1</p>
-                  </div>
-                <div className="column">
-                    <p className="NameTeamM">Column 1</p>
-                  </div>
-                <div className="column">
-                    <p className="NameTeamM">Column 1</p>
-                  </div>
-                <div className="column">
-                    <p className="NameTeamM">Column 1</p>
-                  </div>
-
-
+            <div>
+                <p  className="GapEmail">phusit.mon@student.mahidol.ac.th</p>
+              <p className="GapNum">6588059</p>
             </div>
         </div>
+{/*  */}
+            <div className="column">
+              <div className="Picadjust">
+                <div className="PicFrame">
+                    {/* img Thame */}
+                </div>
+              </div>
+
+                <p className="NameTeamM">Thame</p>
+                  <div className="PersonName">
+                            <p className="GapName">Vichayuth</p>
+                          <p className="GapName">Ngamsitthipong</p>
+                        </div>
+
+                    <div>
+                          <p  className="GapEmail">vichayuth.nga@student.mahidol.ac.th</p>
+                        <p className="GapNum">6588061</p>
+                      </div>
+            </div>
+{/*  */}
+            <div className="column">
+              <div className="Picadjust">
+                <div className="PicFrame">
+                  {/* img Mac */}
+                </div>
+              </div>
+
+                    <p className="NameTeamM">Mac</p>
+                      <div className="PersonName">
+                            <p className="GapName">Wasuntha</p>
+                          <p className="GapName">Phanpanich</p>
+                        </div>
+
+                    <div>
+                          <p  className="GapEmail">wasuntha.pha@student.mahidol.ac.th</p>
+                        <p className="GapNum">6588068</p>
+                      </div>
+            </div>
+{/*  */}
+            <div className="column">
+              <div className="Picadjust">
+                <div className="PicFrame">
+                  {/* img Jui */}
+                </div>
+              </div>
+
+                  <p className="NameTeamM">Jui</p>
+                    <div className="PersonName">
+                        <p className="GapName">Sorawit</p>
+                        <p className="GapName">Piriyapanyaporn </p>
+                  </div>
+
+                    <div>
+                          <p  className="GapEmail">sorawit.pir@student.mahidol.ac.th</p>
+                        <p className="GapNum">6588075</p>
+                      </div>
+                </div>
+{/*  */}
+            <div className="column">
+              <div className="Picadjust">
+                <div className="PicFrame">
+                  {/* img Pran */}
+                </div>
+              </div>
+
+                  <div>
+                    <p className="NameTeamM">Pran</p>
+                      <div className="PersonName">
+                            <p className="GapName">Punnathorn</p>
+                          <p className="GapName">Laohachote</p>
+                        </div>
+
+                    <div>
+                          <p  className="GapEmail">punnathorn.lao@student.mahidol.ac.th</p>
+                        <p className="GapNum">6588179</p>
+                      </div>
+                  
+                  </div>
+                    
+
+            </div>
+
+
+          </div>
+      </div>
       
 
-    </body>
+  </body>
 
     <footer className="footerbar">
 
@@ -64,8 +147,6 @@ function Aboutus(){
     
     </footer>
       
-
-
     </>
   )
 }
