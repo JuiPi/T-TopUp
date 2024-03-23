d444c99116fbafa4ea783eebc9e65bd8c7ad4cc1
PUNNAPRAN <109496465+PUNNAPRAN@users.noreply.github.com>
2024-03-24 02:05:17 +0700

    Add User Management

--- T-TopUp/src/App.tsx
+++ T-TopUp/src/App.tsx
@@ -5,16 +5,18 @@ import viteLogo from "/vite.svg";
 import Aboutus from "./Aboutus";
 import Homepage from "./Homepage";
 import ProdMng from "./ProdMng";
+import AddADMIN from "./AddADMIN";
+
 import './Homepage.css'
 import './Aboutus.css'
 import './ProdMng.css'
-
+import './AddADMIN.css'
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <ProdMng />
+      <App />
     </>
   );
 }
