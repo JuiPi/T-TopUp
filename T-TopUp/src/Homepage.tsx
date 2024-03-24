3cf6681c34f0a4f29aca66e61d2a8c1dd2fde534
VN140 <vichayuth.nga@student.mahidol.ac.th>
2024-03-25 00:21:49 +0700

    Homepage

--- T-TopUp/src/Homepage.tsx
+++ T-TopUp/src/Homepage.tsx
@@ -3,6 +3,14 @@ import ad2 from "./assets/Game_icon/TF2/tf2_banner.png";
 import ad3 from "./assets/Game_icon/OW/ow_banner.png";
 import NavBar from "./Components/NavBar";
 import Footer from "./Components/Footer";
+import ff from "./assets/Game_icon/Fifa/ff_icon.png";
+import lol from "./assets/Game_icon/LOL/lol_icon.png";
+import ow from "./assets/Game_icon/OW/ow_icon.png";
+import rov from "./assets/Game_icon/ROV/rov_icon.png";
+import tf2 from "./assets/Game_icon/TF2/tf2_icon.png";
+import tft from "./assets/Game_icon/TFT/tft_icon.png";
+import val from "./assets/Game_icon/Val/val_icon.png";
+import rb from "./assets/Game_icon/Roblox/rob_icon.png";
 
 function Homepage() {
   return (
@@ -28,55 +36,56 @@ function Homepage() {
           </div>
         </article>
       </body>
-      <h1>All Games</h1>
-      <article className="container">
+      <br></br>
+      <p className="text">All Games</p>
+      <article className="gamecontainer">
         <article className="gamecolumn">
           <section className="imggame">
-            <img src="logogame.png"></img>
+            <img src={ff} className="gameicon"></img>
+          </section>
+          <section className="gamename">FIFA 2019</section>
+        </article>
+        <article className="gamecolumn">
+          <section className="imggame">
+            <img src={ow} className="gameicon"></img>
+          </section>
+          <section className="gamename">Overwatch</section>
+        </article>
+        <article className="gamecolumn">
+          <section className="imggame">
+            <img src={lol} className="gameicon"></img>
+          </section>
+          <section className="gamename">Leagues of Legend</section>
+        </article>
+        <article className="gamecolumn">
+          <section className="imggame">
+            <img src={rov} className="gameicon"></img>
+          </section>
+          <section className="gamename">Arena of Valor</section>
+        </article>
+        <article className="gamecolumn">
+          <section className="imggame">
+            <img src={tf2} className="gameicon"></img>
+          </section>
+          <section className="gamename">Team Fortress 2</section>
+        </article>
+        <article className="gamecolumn">
+          <section className="imggame">
+            <img src={tft} className="gameicon"></img>
+          </section>
+          <section className="gamename">Teamfight Tactics</section>
+        </article>
+        <article className="gamecolumn">
+          <section className="imggame">
+            <img src={val} className="gameicon"></img>
           </section>
           <section className="gamename">Valorant</section>
         </article>
         <article className="gamecolumn">
           <section className="imggame">
-            <img src="logogame.png"></img>
+            <img src={rb} className="gameicon"></img>
           </section>
-          <section className="gamename">Valorant</section>
-        </article>
-        <article className="gamecolumn">
-          <section className="imggame">
-            <img src="logogame.png"></img>
-          </section>
-          <section className="gamename">Valorant</section>
-        </article>
-        <article className="gamecolumn">
-          <section className="imggame">
-            <img src="logogame.png"></img>
-          </section>
-          <section className="gamename">Valorant</section>
-        </article>
-        <article className="gamecolumn">
-          <section className="imggame">
-            <img src="logogame.png"></img>
-          </section>
-          <section className="gamename">Valorant</section>
-        </article>
-        <article className="gamecolumn">
-          <section className="imggame">
-            <img src="logogame.png"></img>
-          </section>
-          <section className="gamename">Valorant</section>
-        </article>
-        <article className="gamecolumn">
-          <section className="imggame">
-            <img src="logogame.png"></img>
-          </section>
-          <section className="gamename">Valorant</section>
-        </article>
-        <article className="gamecolumn">
-          <section className="imggame">
-            <img src="logogame.png"></img>
-          </section>
-          <section className="gamename">Valorant</section>
+          <section className="gamename">Roblox</section>
         </article>
       </article>
       <Footer />
