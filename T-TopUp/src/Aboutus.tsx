9d5056ba367d748e6f22d499458965e5cddaa687
sup14e <luckymackie7794@gmail.com>
2024-03-24 17:19:44 +0700

    -Resize used image files and display it -Added hovering on games in ProdMng

--- T-TopUp/src/Aboutus.tsx
+++ T-TopUp/src/Aboutus.tsx
@@ -1,133 +1,120 @@
 import NavBar from "./Components/NavBar";
 import Footer from "./Components/Footer";
+import jui from "./assets/About_Us/jui.png";
+import mac from "./assets/About_Us/mac.png"
+import thames from "./assets/About_Us/thames.png"
+import term from "./assets/About_Us/term.png"
+import pran from "./assets/About_Us/pran.png"
 
-function Aboutus(){
-  return(
+function Aboutus() {
+  return (
     <>
-    <header>
-        <NavBar/>
-    </header>
-{/*  */}
-  <body>
-      <div>
-          <p className="TeamMem">
-            Our Team Members
-          </p>
-      </div>
+      <header>
+        <NavBar />
+      </header>
+      {/*  */}
+      <body>
+        <div>
+          <p className="TeamMem">Our Team Members</p>
+        </div>
 
-      <div className="container">
-            <div className="grid-container">
-{/*  */}
-        <div className="column">
-          <div className="Picadjust">
-            <div className="PicFrame">
-              {/* img Term */}
-            </div>
-          </div>
-            <p className="NameTeamM">Term</p>
+        <div className="container">
+          <div className="grid-container">
+            {/*  */}
+            <div className="column">
+              <div className="Picadjust">
+                <img className="PicFrame" src={term}></img>
+              </div>
+              <p className="NameTeamM">Term</p>
               <div className="PersonName">
                 <p className="GapName">Phusit</p>
-                  <p className="GapName">Mongkhonwatcharaphun</p>
+                <p className="GapName">Mongkhonwatcharaphun</p>
               </div>
 
-            <div>
-                <p  className="GapEmail">phusit.mon@student.mahidol.ac.th</p>
-              <p className="GapNum">6588059</p>
+              <div>
+                <p className="GapEmail">phusit.mon@student.mahidol.ac.th</p>
+                <p className="GapNum">6588059</p>
+              </div>
             </div>
-        </div>
-{/*  */}
+            {/*  */}
             <div className="column">
               <div className="Picadjust">
-                <div className="PicFrame">
-                    {/* img Thame */}
-                </div>
+                <img className="PicFrame" src={thames}></img>
               </div>
 
-                <p className="NameTeamM">Thame</p>
-                  <div className="PersonName">
-                            <p className="GapName">Vichayuth</p>
-                          <p className="GapName">Ngamsitthipong</p>
-                        </div>
+              <p className="NameTeamM">Thame</p>
+              <div className="PersonName">
+                <p className="GapName">Vichayuth</p>
+                <p className="GapName">Ngamsitthipong</p>
+              </div>
 
-                    <div>
-                          <p  className="GapEmail">vichayuth.nga@student.mahidol.ac.th</p>
-                        <p className="GapNum">6588061</p>
-                      </div>
+              <div>
+                <p className="GapEmail">vichayuth.nga@student.mahidol.ac.th</p>
+                <p className="GapNum">6588061</p>
+              </div>
             </div>
-{/*  */}
+            {/*  */}
             <div className="column">
               <div className="Picadjust">
-                <div className="PicFrame">
-                  {/* img Mac */}
-                </div>
+                <img className="PicFrame" src={mac}></img>
               </div>
 
-                    <p className="NameTeamM">Mac</p>
-                      <div className="PersonName">
-                            <p className="GapName">Wasuntha</p>
-                          <p className="GapName">Phanpanich</p>
-                        </div>
+              <p className="NameTeamM">Mac</p>
+              <div className="PersonName">
+                <p className="GapName">Wasuntha</p>
+                <p className="GapName">Phanpanich</p>
+              </div>
 
-                    <div>
-                          <p  className="GapEmail">wasuntha.pha@student.mahidol.ac.th</p>
-                        <p className="GapNum">6588068</p>
-                      </div>
+              <div>
+                <p className="GapEmail">wasuntha.pha@student.mahidol.ac.th</p>
+                <p className="GapNum">6588068</p>
+              </div>
             </div>
-{/*  */}
+            {/*  */}
             <div className="column">
               <div className="Picadjust">
-                <div className="PicFrame">
-                  {/* img Jui */}
-                </div>
+                <img className="PicFrame" src={jui}></img>
               </div>
 
-                  <p className="NameTeamM">Jui</p>
-                    <div className="PersonName">
-                        <p className="GapName">Sorawit</p>
-                        <p className="GapName">Piriyapanyaporn </p>
-                  </div>
+              <p className="NameTeamM">Jui</p>
+              <div className="PersonName">
+                <p className="GapName">Sorawit</p>
+                <p className="GapName">Piriyapanyaporn </p>
+              </div>
 
-                    <div>
-                          <p  className="GapEmail">sorawit.pir@student.mahidol.ac.th</p>
-                        <p className="GapNum">6588075</p>
-                      </div>
-                </div>
-{/*  */}
+              <div>
+                <p className="GapEmail">sorawit.pir@student.mahidol.ac.th</p>
+                <p className="GapNum">6588075</p>
+              </div>
+            </div>
+            {/*  */}
             <div className="column">
               <div className="Picadjust">
-                <div className="PicFrame">
-                  {/* img Pran */}
-                </div>
+                <img className="PicFrame" src={pran}></img>
               </div>
 
-                  <div>
-                    <p className="NameTeamM">Pran</p>
-                      <div className="PersonName">
-                            <p className="GapName">Punnathorn</p>
-                          <p className="GapName">Laohachote</p>
-                        </div>
+              <div>
+                <p className="NameTeamM">Pran</p>
+                <div className="PersonName">
+                  <p className="GapName">Punnathorn</p>
+                  <p className="GapName">Laohachote</p>
+                </div>
 
-                    <div>
-                          <p  className="GapEmail">punnathorn.lao@student.mahidol.ac.th</p>
-                        <p className="GapNum">6588179</p>
-                      </div>
-                  
-                  </div>
-                    
-
+                <div>
+                  <p className="GapEmail">
+                    punnathorn.lao@student.mahidol.ac.th
+                  </p>
+                  <p className="GapNum">6588179</p>
+                </div>
+              </div>
             </div>
-
-
           </div>
-      </div>
-      
+        </div>
+      </body>
 
-  </body>
-
-    <Footer/>
-      
+      <Footer />
     </>
-  )
+  );
 }
 
 export default Aboutus;
--- T-TopUp/src/ProdMng.tsx
+++ T-TopUp/src/ProdMng.tsx
@@ -1,83 +1,98 @@
 import NavBar from "./Components/NavBar";
 import Footer from "./Components/Footer";
+import ff from "./assets/Game_icon/Fifa/ff_icon.png";
+import lol from "./assets/Game_icon/LOL/lol_icon.png";
+import ow from "./assets/Game_icon/OW/ow_icon.png";
+import rov from "./assets/Game_icon/ROV/rov_icon.png";
+import tf2 from "./assets/Game_icon/TF2/tf2_icon.png";
+import tft from "./assets/Game_icon/TFT/tft_icon.png";
+import val from "./assets/Game_icon/Val/val_icon.png";
+import rb from "./assets/Game_icon/Roblox/rob_icon.png";
+import addg from "./assets/Misc/add_game.png";
+import edit from "./assets/Misc/Edit_Cover.png";
 
 function ProdMng() {
-    return (
-      <>
-        <header>
-          <NavBar />
-        </header>
-  
-        <body>
-          <p className="title">Product Management</p>
-          <p className="allG">All Games</p>
-          {/* <div className="display"> */}
-  
-              <div className="game-container">
-  
-                  <div className="game">
-                      <div id="addgame" className="gameIcon"></div>
-                      <p>game name</p>
-                  </div>
-  
-                  <div className="game">
-                      <div className="gameIcon"></div>
-                      <div className="close">X</div>
-                  <p>game name</p>
-                  </div>
-  
-                  <div className="game">
-                      <div className="gameIcon"></div>
-                      <div className="close">X</div>
-                  <p>game name</p>
-                  </div>
-  
-                  <div className="game">
-                      <div className="gameIcon"></div>
-                      <div className="close">X</div>
-                  <p>game name</p>
-                  </div>
-  
-                  <div className="game">
-                      <div className="gameIcon"></div>
-                      <div className="close">X</div>
-                  <p>game name</p>
-                  </div>
-  
-                  <div className="game">
-                      <div className="gameIcon"></div>
-                      <div className="close">X</div>
-                  <p>game name</p>
-                  </div>
-  
-                  <div className="game">
-                      <div className="gameIcon"></div>
-                      <div className="close">X</div>
-                  <p>game name</p>
-                  </div>
-  
-                  <div className="game">
-                      <div className="gameIcon"></div>
-                      <div className="close">X</div>
-                  <p>game name</p>
-                  </div>
-  
-                  <div className="game">
-                      <div className="gameIcon"></div>
-                      <div className="close">X</div>
-                  <p>game name</p>
-                  </div>
-  
-            </div>
-          {/* </div> */}
-        </body>
-  
-        <footer className="footerbar">
-          <Footer />
-        </footer>
-      </>
-    );
-  }
-  
-  export default ProdMng;
-  
+  return (
+    <>
+      <header>
+        <NavBar />
+      </header>
+
+      <body>
+        <p className="title">Product Management</p>
+        <p className="allG">All Games</p>
+        {/* <div className="display"> */}
+
+        <div className="game-container">
+          <div className="game">
+            <img className="gameIcon" src={addg}></img>
+            <p>game name</p>
+          </div>
+
+          <div className="game">
+            <img className="gameIcon" src={ff}></img>
+            <img className="editIcon" src={edit}></img>
+            <div className="close">X</div>
+            <p>Fifa</p>
+          </div>
+
+          <div className="game">
+            <img className="gameIcon" src={lol}></img>
+            <img className="editIcon" src={edit}></img>
+            <div className="close">X</div>
+            <p>League of Legends</p>
+          </div>
+
+          <div className="game">
+            <img className="gameIcon" src={ow}></img>
+            <img className="editIcon" src={edit}></img>
+            <div className="close">X</div>
+            <p>Overwatch</p>
+          </div>
+
+          <div className="game">
+            <img className="gameIcon" src={rov}></img>
+            <img className="editIcon" src={edit}></img>
+            <div className="close">X</div>
+            <p>ROV</p>
+          </div>
+
+          <div className="game">
+            <img className="gameIcon" src={tf2}></img>
+            <img className="editIcon" src={edit}></img>
+            <div className="close">X</div>
+            <p>Team Fortress 2</p>
+          </div>
+
+          <div className="game">
+            <img className="gameIcon" src={tft}></img>
+            <img className="editIcon" src={edit}></img>
+            <div className="close">X</div>
+            <p>Teamfight Tactics</p>
+          </div>
+
+          <div className="game">
+            <img className="gameIcon" src={val}></img>
+            <img className="editIcon" src={edit}></img>
+            <div className="close">X</div>
+            <p>Valorant</p>
+          </div>
+
+          <div className="game">
+            <img className="gameIcon" src={rb}></img>
+            <img className="editIcon" src={edit}></img>
+            <div className="close">X</div>
+            <p>Roblox</p>
+          </div>
+        </div>
+        {/* </div> */}
+      </body>
+
+      <footer className="footerbar">
+        <Footer />
+      </footer>
+    </>
+  );
+}
+
+export default ProdMng;
