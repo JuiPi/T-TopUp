c3f437b740d24a5f351b1052f7fa5b73b78a3998
term04 <156349764+term04@users.noreply.github.com>
2024-03-29 01:25:18 +0700

    Add router
    Now you can go to page via router see path in route.tsx

--- T-TopUp/src/App.tsx
+++ T-TopUp/src/App.tsx
@@ -2,27 +2,35 @@ import { useState } from "react";
 // import reactLogo from "./assets/react.svg";
 // import viteLogo from "/vite.svg";
 // import "./App.css";
+import React from "react";
+
 import Aboutus from "./Aboutus";
 import Homepage from "./Homepage";
 import ProdMng from "./ProdMng";
 import AddADMIN from "./AddADMIN";
 import UserManagement from "./UserManagement";
 import EditADMIN from "./EditADMIN";
+import { Outlet } from 'react-router-dom';
 
 
-function App() {
-  const [count, setCount] = useState(0);
 
-  return (
-    <>
-      {/* <Homepage /> */}
-      {/* <Aboutus /> */}
-      {/* <ProdMng /> */}
-      {/* <UserManagement /> */}
-      {/* <AddADMIN /> */}
-      <EditADMIN/>
-    </>
+
+function App(){
+  
+  return(<>
+  <Outlet/>
+  </>
   );
+  // return (
+  //   <>
+  //     {/* <Homepage /> */}
+  //     {/* <Aboutus /> */}
+  //     {/* <ProdMng /> */}
+  //     {/* <UserManagement /> */}
+  //     {/* <AddADMIN /> */}
+  //     <EditADMIN/>
+  //   </>
+  // );
 }
 
 export default App;
