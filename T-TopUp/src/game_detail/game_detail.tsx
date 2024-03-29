efd7af90bba25e7731203e03dd8b60fa1230f5b3
term04 <156349764+term04@users.noreply.github.com>
2024-03-29 13:55:35 +0700

    add game_detail file
    just template for game detail and add route to gamedetail

---
+++ T-TopUp/src/game_detail/game_detail.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import NavBar from '../Components/NavBar';
+import Footer from '../Components/Footer';
+
+
+type Props = {}
+
+const GameDetail = (props: Props) => {
+  return (
+    <>
+    <header>
+        <NavBar/>
+    </header>
+    <body>
+        <div className="container xl:mx-auto">
+            <div>
+
+            </div>
+
+        </div>
+        
+    </body>
+    <Footer />
+    </> 
+  );
+}
+
+export default GameDetail;
