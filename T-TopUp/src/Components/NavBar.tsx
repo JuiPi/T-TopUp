2628048e4f03cf6ca0d011937506108d0ad20f7a
sup14e <luckymackie7794@gmail.com>
2024-03-30 17:18:09 +0700

    Update 03-30-24 - Fixed Search Bar and added Adv. Search icon - Fixed Game layout in both Homepage and ProdMng

--- T-TopUp/src/Components/NavBar.tsx
+++ T-TopUp/src/Components/NavBar.tsx
@@ -1,25 +1,27 @@
 import picture from "../assets/Misc/logo.png";
-import "./NavBar.css"
+import mag from "../assets/Misc/search.png";
+import "./NavBar.css";
 
 function NavBar() {
   return (
     <>
       <nav className="nav1">
-        <a href="#">Log in</a>
+        <a href="log-in">Log in</a>
       </nav>
       <switch className="nav2">
         <a href="Homepage">
           <img src={picture} alt="Logo" className="logosize"></img>
         </a>
-        <a href="Homepage">Home</a>
-        <a href="Games">Games</a>
-        <a href="UserManagement">User Management</a>
-        <a href="ProductManagement">Product Management</a>
+        <a href="/">Home</a>
+        <a href="#">Games</a>
+        <a href="admin-management">User Management</a>
+        <a href="product-management">Product Management</a>
         <div className="search">
-            <form action="#">
-                <input type="text" placeholder="Search" name="search"></input>
-                {/* <button type="submit">Search</button> */}
-            </form>
+          <form action="#">
+            <input type="text" placeholder="Search" name="search"></input>
+            {/* <button type="submit">Search</button> */}
+          </form>
+          <img src={mag}></img>
         </div>
       </switch>
     </>
