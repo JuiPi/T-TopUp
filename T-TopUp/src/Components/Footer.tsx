9864bc21cc954dd8b14c05f7f69961871560a331
PUNNAPRAN <109496465+PUNNAPRAN@users.noreply.github.com>
2024-03-31 22:10:05 +0700

    UPDATE FOOTER

--- T-TopUp/src/Components/Footer.tsx
+++ T-TopUp/src/Components/Footer.tsx
@@ -8,14 +8,23 @@ function Footer() {
     <>
       <footer className="footerbar">
         <div className="circle-container">
+          
           <div className="circle">
-            <img src={fb}></img>
+            <a href="https://www.facebook.com/photo/?fbid=1784085094969118&set=pob.100002453928010&locale=th_TH">
+              <img src={fb}></img>              
+            </a>
           </div>
+
           <div className="circle">
-            <img src={ig}></img>
+            <a href="https://www.instagram.com/p/CysztxKLSf9AZYTleelYxCVFEdD38-KFL8N4440/?img_index=5">
+              <img src={ig}></img>              
+            </a>
           </div>
+
           <div className="circle">
-            <img src={twt}></img>
+            <a href="https://twitter.com/LFC">
+              <img src={twt}></img>
+            </a>
           </div>
         </div>
 
