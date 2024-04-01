7e9b6ea3f0783374174a1afd323d9f76103ca295
VN140 <vichayuth.nga@student.mahidol.ac.th>
2024-04-01 16:23:29 +0700

    Search Result Update

--- T-TopUp/src/SearchResult.tsx
+++ T-TopUp/src/SearchResult.tsx
@@ -1,44 +1,45 @@
 import NavBar from "./Components/NavBar";
 import Footer from "./Components/Footer";
-import './SearchResult.css'
+import "./SearchResult.css";
+import X from "./assets/Misc/closeX.png";
 
 function SearchResult() {
-    return (
-        <>
-        <header>
-            <NavBar />
-        </header>
+  return (
+    <>
+      <header>
+        <NavBar />
+      </header>
 
-        <body>
-            <div className="filter-content">
-                <h1>Search Result(s)</h1>
-                <hr></hr>
-                <div className="search-filter">
-                    <div className="platform">
-                        <p>x</p>
-                        <p>Platform: PC</p>
-                    </div>
-                    <div className="Genre">
-                        <p>x</p>
-                        <p>Genre: FPS</p>
-                    </div>
-                    <div className="Publisher">
-                        <p>x</p>
-                        <p>Publisher: Any</p>
-                    </div>
-                    <div className="Promotion">
-                        <p>x</p>
-                        <p>Promotion Only</p>
-                    </div>
-                </div>
+      <body>
+        <div className="filter-content">
+          <h1>Search Result(s)</h1>
+          <hr></hr>
+          <div className="search-filter">
+            <div className="platform">
+                <img src={X}></img>
+                <p>Platform: PC</p>
             </div>
-        </body>
+            <div className="Genre">
+                <img src={X}></img>
+                <p>Genre: FPS</p>
+            </div>
+            <div className="Publisher">
+                <img src={X}></img>
+                <p>Publisher: Any</p>
+            </div>
+            <div className="Promotion">
+                <img src={X}></img>
+                <p>Promotion Only</p>
+            </div>
+          </div>
+        </div>
+      </body>
 
-        <footer>
-            <Footer />
-        </footer>
-        </>
-    );
+      <footer>
+        <Footer />
+      </footer>
+    </>
+  );
 }
 
 export default SearchResult;
